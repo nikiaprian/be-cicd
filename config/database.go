a4148fb199ad82aadbec7ac42d8d3718b88c4c99
nikiaprian <aprianniki69@gmail.com>
2024-01-18 14:26:41 +0700

    fourth

--- config/database.go
+++ config/database.go
@@ -10,7 +10,7 @@ import (
 
 func NewPostgreSQLDB() (*services.PostgreSQL, error) {
 	connectionString := fmt.Sprintf(
-		"host=18.236.220.17.47 port=5432 user=postgres password=nikiskripsi dbname=postgres sslmode=disable",
+		"host=54.202.114.22 port=5432 user=postgres password=nikiskripsi dbname=postgres sslmode=disable",
 	)
 
 	db, err := sql.Open("postgres", connectionString)
