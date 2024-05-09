1368e437b57eb922d28cb05ea00d51d4cf4bc496
nikiaprian <101971713+nikiaprian@users.noreply.github.com>
2024-05-10 02:38:26 +0700

    Update database.go

--- config/database.go
+++ config/database.go
@@ -10,7 +10,7 @@ import (
 
 func NewPostgreSQLDB() (*services.PostgreSQL, error) {
 	connectionString := fmt.Sprintf(
-		"host=docker-alb-be-1961992740.us-west-2.elb.amazonaws.com port=5432 user=postgres password=nikiskripsi dbname=postgres sslmode=disable",
+		"host=10.10.3.244 port=5432 user=postgres password=nikiskripsi dbname=postgres sslmode=disable",
 	)
 
 	db, err := sql.Open("postgres", connectionString)
