891894a4d9d22e30f802eee2021d42125e28629b
nikiaprian <aprianniki69@gmail.com>
2024-08-21 00:04:52 +0700

    new20aug32

--- project/base.go
+++ project/base.go
@@ -10,11 +10,6 @@ type Project struct {
 	Storage *storage.StorageS3Stuct
 }
 
-type ProjectInterface interface {
-    Usecase() UsecaseInterface // Pastikan ini sesuai dengan struktur Anda
-    Storage() StorageInterface   // Jika ada Storage
-}
-
 func NewProject(usecase *usecase.Usecase, sto storage.StorageS3Stuct) *Project {
 	return &Project{Usecase: usecase, Storage: &sto}
 }
