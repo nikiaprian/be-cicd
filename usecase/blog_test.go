5977d029f17fe13d58280d291935656b03d49333
nikiaprian <aprianniki69@gmail.com>
2024-08-21 01:51:41 +0700

    new20aug42

---
+++ usecase/blog_test.go
@@ -0,0 +1,48 @@
+package usecase
+
+import (
+    "context"
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+    "codein/models"
+    "codein/repository"
+    "codein/tests"
+)
+
+type MockRepository struct {
+    mock.Mock
+}
+
+func (m *MockRepository) CreateBlog(ctx context.Context, blog models.Blog) (*models.Blog, error) {
+    args := m.Called(ctx, blog)
+    return args.Get(0).(*models.Blog), args.Error(1)
+}
+
+func TestCreateBlogUsecase(t *testing.T) {
+    ctrl := gomock.NewController(t)
+    defer ctrl.Finish()
+
+    mockRepo := NewMockRepository(ctrl)
+
+    expectedBlog := models.Blog{
+        Title:   "Test Blog",
+        Content: "This is a test blog",
+    }
+
+    mockRepo.EXPECT().CreateBlog(context.Background(), models.Blog{
+        Title:   "Test Blog",
+        Content: "This is a test blog",
+    }).Return(&expectedBlog, nil)
+
+    usecase := NewUsecase(mockRepo)
+
+    blog, err := usecase.CreateBlog(context.Background(), models.Blog{
+        Title:   "Test Blog",
+        Content: "This is a test blog",
+    })
+
+    assert.NoError(t, err)
+    assert.Equal(t, expectedBlog, *blog)
+    mockRepo.AssertExpectations(t)
+}
