84781fc88998a4ee153afebf12db59c6a399722c
nikiaprian <aprianniki69@gmail.com>
2024-08-20 23:46:20 +0700

    new20aug26

--- repository/mock_repository.go
+++ repository/mock_repository.go
@@ -25,4 +25,14 @@ func (m *MockRepository) GetAllLikeByBlogID(ctx *gin.Context, id int) (*[]models
 	return args.Get(0).(*[]models.BlogsLikesResponse), args.Error(1)
 }
 
+func (m *MockRepository) GetLikeByUserIDAndBlogID(ctx *gin.Context, user_id, blog_id int) (*models.BlogsLikesResponse, error) {
+	args := m.Called(ctx, user_id, blog_id)
+	return args.Get(0).(*models.BlogsLikesResponse), args.Error(1)
+}
+
+func (m *MockRepository) DeleteLikeByBlogId(ctx *gin.Context, user_id, blog_id int) (*models.BlogsLikesResponse, error) {
+	args := m.Called(ctx, user_id, blog_id)
+	return args.Get(0).(*models.BlogsLikesResponse), args.Error(1)
+}
+
 // Tambahkan method lainnya sesuai kebutuhan pengujian
