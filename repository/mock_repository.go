4762dbbbd5cb5b815f1afd72485f9ee8ad601736
nikiaprian <aprianniki69@gmail.com>
2024-08-22 00:19:42 +0700

    new22aug4

--- repository/mock_repository.go
+++ repository/mock_repository.go
@@ -6,67 +6,33 @@ import (
 	"codein/models"
 )
 
-// MockRepository adalah struct untuk mock objek repository yang digunakan dalam pengujian
 type MockRepository struct {
 	mock.Mock
 }
 
-// Metode untuk mock pada repository/bloglike.go
+func (m *MockRepository) CreateBlog(ctx *gin.Context, blogReq models.BlogRequest, photo string, userID int) (*models.Blog, error) {
+	args := m.Called(ctx, blogReq, photo, userID)
+	return args.Get(0).(*models.Blog), args.Error(1)
+}
 
 func (m *MockRepository) CreateLikeByBlogId(ctx *gin.Context, user_id, blog_id int) (*models.BlogsLikesResponse, error) {
 	args := m.Called(ctx, user_id, blog_id)
 	return args.Get(0).(*models.BlogsLikesResponse), args.Error(1)
 }
 
-// Tambahkan method lainnya sesuai kebutuhan pengujian untuk bloglike.go
-
-// Metode untuk mock pada repository/blogcommentlike.go
-
-func (m *MockRepository) CreateLikeByBlogCommentId(ctx *gin.Context, user_id, blog_comment_id int) (*models.BlogCommentLikeResponse, error) {
-	args := m.Called(ctx, user_id, blog_comment_id)
-	return args.Get(0).(*models.BlogCommentLikeResponse), args.Error(1)
+func (m *MockRepository) GetAllLikeByBlogID(ctx *gin.Context, id int) (*[]models.BlogsLikesResponse, error) {
+	args := m.Called(ctx, id)
+	return args.Get(0).(*[]models.BlogsLikesResponse), args.Error(1)
 }
 
-func (m *MockRepository) GetAllLikeByBlogCommentID(ctx *gin.Context, blog_comment_id int) (*[]models.BlogCommentLikeResponse, error) {
-	args := m.Called(ctx, blog_comment_id)
-	return args.Get(0).(*[]models.BlogCommentLikeResponse), args.Error(1)
+func (m *MockRepository) GetLikeByUserIDAndBlogID(ctx *gin.Context, user_id, blog_id int) (*models.BlogsLikesResponse, error) {
+	args := m.Called(ctx, user_id, blog_id)
+	return args.Get(0).(*models.BlogsLikesResponse), args.Error(1)
 }
 
-func (m *MockRepository) DeleteLikeByBlogCommentId(ctx *gin.Context, user_id, blog_comment_id int) (*models.BlogCommentLikeResponse, error) {
-	args := m.Called(ctx, user_id, blog_comment_id)
-	return args.Get(0).(*models.BlogCommentLikeResponse), args.Error(1)
+func (m *MockRepository) DeleteLikeByBlogId(ctx *gin.Context, user_id, blog_id int) (*models.BlogsLikesResponse, error) {
+	args := m.Called(ctx, user_id, blog_id)
+	return args.Get(0).(*models.BlogsLikesResponse), args.Error(1)
 }
 
-// Metode untuk mock pada repository/forumcommentlike.go
-
-func (m *MockRepository) CreateLikeByForumCommentId(ctx *gin.Context, user_id, forum_comment_id int) (*models.ForumCommentLikeResponse, error) {
-	args := m.Called(ctx, user_id, forum_comment_id)
-	return args.Get(0).(*models.ForumCommentLikeResponse), args.Error(1)
-}
-
-func (m *MockRepository) GetAllLikeByForumCommentID(ctx *gin.Context, forum_comment_id int) (*[]models.ForumCommentLikeResponse, error) {
-	args := m.Called(ctx, forum_comment_id)
-	return args.Get(0).(*[]models.ForumCommentLikeResponse), args.Error(1)
-}
-
-func (m *MockRepository) DeleteLikeByForumCommentId(ctx *gin.Context, user_id, forum_comment_id int) (*models.ForumCommentLikeResponse, error) {
-	args := m.Called(ctx, user_id, forum_comment_id)
-	return args.Get(0).(*models.ForumCommentLikeResponse), args.Error(1)
-}
-
-// Metode untuk mock pada repository/forumlikes.go
-
-func (m *MockRepository) CreateLikeByForumId(ctx *gin.Context, user_id, forum_id int) (*models.ForumLikesResponse, error) {
-	args := m.Called(ctx, user_id, forum_id)
-	return args.Get(0).(*models.ForumLikesResponse), args.Error(1)
-}
-
-func (m *MockRepository) GetAllLikeByForumID(ctx *gin.Context, forum_id int) (*[]models.ForumLikesResponse, error) {
-	args := m.Called(ctx, forum_id)
-	return args.Get(0).(*[]models.ForumLikesResponse), args.Error(1)
-}
-
-func (m *MockRepository) DeleteLikeByForumId(ctx *gin.Context, user_id, forum_id int) (*models.ForumLikesResponse, error) {
-	args := m.Called(ctx, user_id, forum_id)
-	return args.Get(0).(*models.ForumLikesResponse), args.Error(1)
-}
+// Tambahkan method lainnya sesuai kebutuhan pengujian
